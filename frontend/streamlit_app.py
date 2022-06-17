2eb687d9f5852115234ce1443c735bf3a51a4a75
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2022-06-17 14:50:57 +0530

    Updated Streamlit code

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -2,25 +2,24 @@ import requests
 from PIL import Image
 import streamlit as st
 
-
+# Change this with your deployed app URL
 FASTAPI_ENDPOINT = r"https://veb-fastapi-back.herokuapp.com/predict"
 
 
-st.title("Image Classification Basic")
-st.header("Imagenet Image Classifiation using Resnet-18 model")
-st.text("The ResNet-18 model will return class probablities for all of the 1000 classes present in Imagenet.")
+st.title("Streamlit Application")
+st.header("Image Classifiation using Resnet-18 model.")
+st.text("The model will return confidence scores for the 1000 classes present in ImageNet.\nWe will print the TOP-5 predictions.")
 
 
 with st.form("my-form", clear_on_submit=True):
     uploaded_file = st.file_uploader("Choose an Image ...", type=["jpg", "jpeg", "png"])
     submitted = st.form_submit_button("Predict!")
+    st.write("UPLOADED!")
 
 if submitted and uploaded_file is not None:
-    st.write("UPLOADED!")
     image = Image.open(uploaded_file)
 
-    st.image(image, caption="Uploaded image.")
-    st.text(uploaded_file.name)
+    st.image(image, caption=uploaded_file.name)
 
     st.write("\nClassifying...")
 
