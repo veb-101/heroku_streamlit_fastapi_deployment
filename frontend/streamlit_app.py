c1c1cae2b6398458600bb7b77b836fd7baeab6b6
Vaibhav Singh <vaibhav.singh.3001@gmail.com>
2022-06-17 15:33:15 +0530

    updated streamlit_app file

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -7,7 +7,7 @@ FASTAPI_ENDPOINT = r"https://veb-fastapi-back.herokuapp.com/predict"
 
 
 st.title("Streamlit Application")
-st.header("Image Classifiation using Resnet-18 model.")
+st.header("Image Classification using Resnet-18 model.")
 st.text("The model will return confidence scores for the 1000 classes present in ImageNet.\nWe will print the TOP-5 predictions.")
 
 
@@ -22,13 +22,14 @@ if submitted and uploaded_file is not None:
     st.image(image, caption=uploaded_file.name)
 
     st.write("\nClassifying...")
-
     # ==============================================================
-    send_data = uploaded_file.getvalue()  # byte data
-    file = {"image_data": send_data}
+    # The FastAPI endpoint expects input image in bytes.
+    # Get image byte data.
+    byte_data = uploaded_file.getvalue()
+    file = {"image_data": byte_data}
 
-    resp_post = requests.post(url=FASTAPI_ENDPOINT, files=file)
-    decoded_predictions = resp_post.json()
+    endpoint_response = requests.post(url=FASTAPI_ENDPOINT, files=file)
+    decoded_predictions = endpoint_response.json()
     # ==============================================================
 
     for key, value_dict in decoded_predictions.items():
